dc6bdbd557b72d511a53486610a7e3b050520cec
t1xelL <sidorov_timofey05@mail.ru>
2025-10-06 20:19:45 +0300

    add comment for version l1.3

--- 3/version1/main.go
+++ 3/version1/main.go
@@ -14,18 +14,20 @@ import (
 )
 
 func main() {
+	// определяем количество воркеров
 	var numWorkers int = runtime.NumCPU()
 	if len(os.Args) > 1 {
 		if n, err := strconv.Atoi(os.Args[1]); err == nil && n > 0 {
 			numWorkers = n
 		}
 	}
-
+	// создаем контекст для управления жизненным циклом горутин
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-
+	// канал для передачи данных
 	ch := make(chan string)
 	wg := sync.WaitGroup{}
+	// запуск N воркеров
 	for i := 0; i < numWorkers; i++ {
 		wg.Add(1)
 		go worker(ctx, i, ch, &wg)
@@ -35,6 +37,7 @@ func main() {
 
 	go writer(ctx, ch, &wg)
 
+	// Gracefull showdown
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
@@ -47,22 +50,23 @@ func main() {
 	fmt.Println("end")
 }
 
+// пишет в канал
 func writer(ctx context.Context, ch chan<- string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	counter := 1
 	for {
 		select {
-		case <-ctx.Done():
+		case <-ctx.Done(): // проверка отмены контекста
 			fmt.Println("writer is done")
 			return
-		default:
+		default: // создаем запись
 			data := fmt.Sprintf("Message %d (time: %v)", counter, time.Now().Format("15:04:05"))
 
 			select {
-			case ch <- data:
+			case ch <- data: // пытаемся отправить данные
 				counter++
 			case <-ctx.Done():
-				return
+				return // проверка отмены во время отправки
 			}
 
 			time.Sleep(time.Millisecond * time.Duration(rand.Intn(300)+50))
@@ -70,14 +74,15 @@ func writer(ctx context.Context, ch chan<- string, wg *sync.WaitGroup) {
 	}
 }
 
+// читает из канала
 func worker(ctx context.Context, id int, ch <-chan string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for {
 		select {
-		case <-ctx.Done():
+		case <-ctx.Done(): // отмена контекста
 			fmt.Printf("worker %d finish work\n", id)
 			return
-		case data, ok := <-ch:
+		case data, ok := <-ch: // получаем данные из канала
 			if !ok {
 				fmt.Printf("worker %d close channel\n", id)
 				return
