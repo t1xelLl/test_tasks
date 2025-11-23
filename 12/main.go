3ba6585bfbe5e3e330a3e66ca85aee1f8271c6e0
t1xelLl <sidorov_timofey05@mail.ru>
2025-11-24 00:39:25 +0300

    solved l1.12

--- 12/main.go
+++ 12/main.go
@@ -1,1 +1,22 @@
-package _2
+package main
+
+import "fmt"
+
+func NewSet[T comparable](sl []T) []T {
+	set := make(map[T]struct{})
+
+	for _, v := range sl {
+		set[v] = struct{}{}
+	}
+
+	res := make([]T, 0, len(set))
+	for k := range set {
+		res = append(res, k)
+	}
+	return res
+}
+
+func main() {
+	sl := []string{"cat", "cat", "dog", "cat", "tree"}
+	fmt.Println(NewSet(sl))
+}
